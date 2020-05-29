fbe237c092baa0d0a9f48f8145e62378a235fb5e
programmer-heeney <programmer.heeney@gmail.com>
2020-05-29 15:41:42 +0100

    JS: Calculate

--- main.js
+++ main.js
@@ -4,7 +4,37 @@
 const operators = document.querySelectorAll(".operator");
 operators.forEach((operator) => {
   operator.addEventListener("click", () => {
-    alert("The operator clicked:" + operator.id);
+    switch (operator.id) {
+      case "clear":
+        printHistory("");
+        printOutput("");
+        break;
+
+      case "backspace":
+        printOutput(reverseNumberFormat(getOutput().slice(0, -1)));
+        break;
+
+      default:
+        let output = getOutput();
+        let history = getHistory();
+        if (output == "") {
+          history = history.slice(0, -1) + operator.id;
+          printHistory(history);
+        }
+        if (output != "") {
+          output = reverseNumberFormat(output);
+          history = history + output;
+          if (operator.id == "=") {
+            let result = eval(history);
+            printOutput(result);
+            printHistory("");
+          } else {
+            history = history + operator.id;
+            printHistory(history);
+            printOutput("");
+          }
+        }
+    }
   });
 });
 
@@ -12,7 +42,11 @@ operators.forEach((operator) => {
 const numbers = document.querySelectorAll(".number");
 numbers.forEach((number) => {
   number.addEventListener("click", () => {
-    printOutput(reverseNumberFormat(getOutput()) + number.innerText);
+    let output = reverseNumberFormat(getOutput());
+    if (output != NaN) {
+      output = output + number.innerText;
+      printOutput(output);
+    }
   });
 });
 
@@ -30,13 +64,16 @@ function printOutput(num) {
   document.getElementById("output-value").innerText = getFormattedNumber(num);
 }
 function getFormattedNumber(num) {
-  if (num == "" || num == ".") {
-    return num;
+  if (num == "" || num == "-") {
+    return "";
   }
   const n = Number(num);
   const value = n.toLocaleString("en");
   return value;
 }
 function reverseNumberFormat(num) {
+  if (num == "-") {
+    return num;
+  }
   return Number(num.replace(/,/g, ""));
 }
